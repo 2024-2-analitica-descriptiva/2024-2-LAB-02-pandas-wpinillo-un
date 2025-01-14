0177ff75b9c4e70e0890d22bfb22f40fa235fb8d
wpinillo-un <wpinillo@unal.edu.co>
2025-01-13 19:18:37 -0500

    Desarrollo pregunta_07.py

--- homework/pregunta_07.py
+++ homework/pregunta_07.py
@@ -20,3 +20,12 @@ def pregunta_07():
     E    67
     Name: c2, dtype: int64
     """
+    import pandas as pd
+
+    path = 'files/input/tbl0.tsv'
+
+    df = pd.read_csv(path, sep='\t')
+    
+    sum_c2 = df.groupby('c1')['c2'].sum()
+
+    return sum_c2
